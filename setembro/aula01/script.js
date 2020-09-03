1ce69c748ea5fc6041b71b18fdebc31195937edb
Dr. Vulpes <61910532+Rotfuchs-von-Vulpes@users.noreply.github.com>
2020-09-03 16:06:30 -0400

    Add files via upload
    uso simples de manipulação de elementos.

---
+++ setembro/aula01/script.js
@@ -0,0 +1,14 @@
+var d = new Date
+console.log(d.getDate());
+
+function trocar(e){
+    e.innerHTML = "Obrigado";
+}
+
+function voltar(e){
+    e.innerHTML = "De novo!";
+}
+
+function test(e){
+    console.log(e.value);
+}
